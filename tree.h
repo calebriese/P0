54ddb1d62eb6e3bda1e5a5e833dfea2923c65108
Caleb Riese <calebriese@outlook.com>
2021-02-09 02:31:16 -0600

    fdsf

--- tree.h
+++ tree.h
@@ -1,8 +1,10 @@
+#include <stdio.h> // do i need this
+#include "node.h"// do i need this too?
+
+
 #ifndef PROJECTZERO_TREE_H
 #define PROJECTZERO_TREE_H
 
-#include <stdio.h> // do i need this
-#include "node.h"// do i need this too?
 Node *  insertNode(char word[6], Node * existingNode);
 Node * createNode(char word[]);
 Node * buildTree(FILE * myfile);
@@ -10,4 +12,5 @@ void printInorder(Node * root, int level, char outName[256]);
 void printPreorder(Node * root, int level, char outName[256]);
 void printPostorder(Node * root, int level, char outName[256]);
 void printDisplay(Node * root, int level, char outName[256]);
+
 #endif
